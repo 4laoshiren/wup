3884705224ddb25d0b83160ac4adb337977617c1
zhongzhong <2322916463@qq.com>
2026-02-23 10:22:59 +0800

    remove debug files

--- bin/test-npm.js
+++
@@ -1,7 +0,0 @@
-import { npm_pkg_check_outdated, npm_pkg_update } from "../src/managers/-npm.js";
-
-const outdated_pkgs = await npm_pkg_check_outdated();
-console.log(outdated_pkgs);
-
-const update_result = await npm_pkg_update("supabase");
-console.log(update_result);
